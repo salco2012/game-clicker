c5cf5bd190eaacbabc5965ebed8fd06ba5e80e94
Igor Solodovnick <info@pro-landing.site>
2022-02-05 22:32:44 +0300

    Подготовка игрового пространства

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
-
+    <play-space />
   </div>
 </template>
 
 <script>
+import PlaySpace from './components/PlaySpace.vue';
 
 export default {
   name: "App",
   components: {
+    PlaySpace,
   },
 };
 </script>
 
 <style lang="scss">
-
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
 </style>
---
+++ src/components/PlaySpace.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="play-space">
+    <div class="balans">{{ balans }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      balans: 0,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.play-space {
+  padding-top: 50px;
+  background-image: url('../assets/img/backround.jpg');
+  min-height: calc(100vh - 50px);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+
+.balans {
+  margin: 0 auto;
+  position: relative;
+  height: 40px;
+  width: 300px;
+  background-color: #0000004d;
+  border-radius: 20px;
+  box-shadow: 2px 3px rgba(0, 0, 0, 0.562);
+  font-size: 25px;
+  color: white;
+  font-weight: bold;
+  text-shadow: 2px 2px black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.balans::before {
+  content: '';
+  position: absolute;
+  left: -10px;
+  top: -5px;
+  width: 50px;
+  height: 50px;
+  background-image: url('../assets/img/gold-coin.png');
+  background-size: 50px 50px;
+}
+</style>
