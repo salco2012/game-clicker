128815655be1d2a3adcb28df8403fa59481ef0b5
Igor Solodovnick <info@pro-landing.site>
2022-02-07 16:39:13 +0300

    Отрендерил список бизнесов и добавил аудио при покупке

--- src/components/BusinessPassiveIncome.vue
+++ src/components/BusinessPassiveIncome.vue
@@ -2,29 +2,55 @@
   <div class="business">
     <img
       class="business__img"
-      src="../assets/img/businessPassive/mine.jpg"
-      alt="mine"
+      :src="require(`../assets/img/businessPassive/${img}`)"
     />
     <div class="business__info">
-      <h2 class="business__title">Угольная шахта</h2>
-      <p class="business__description">+ 20 монет <br />к пассивному доходу</p>
-      <p class="business__price">
-        Стоимость: <span class="business__price-bold">100 монет</span>
+      <h2 class="business__title">{{ title }}</h2>
+      <p class="business__description">
+        + {{ income }} монет <br/> <span class="business__description-black">к пассивному доходу</span>
       </p>
-      <button class="business__buy">Купить</button>
+      <div class="business__acquisition">
+        <p class="business__price">
+          Стоимость: <span class="business__price-bold">{{ price }} монет</span>
+        </p>
+        <button class="business__buy" @click="$emit('byu-business', $event)">Купить</button>
+      </div>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  props: {
+    img: {
+      typeof: String,
+      require: true,
+    },
+    title: {
+      typeof: String,
+    },
+    income: {
+      typeof: Number,
+      default: '-',
+    },
+    price: {
+      typeof: Number,
+      default: '-',
+    },
+  },
+};
+</script>
+
 <style lang="scss" scoped>
 .business {
-  margin-left: 20px;
-  width: 550px;
-  height: 150px;
+  width: 580px;
+  height: 180px;
   background-color: rgba(255, 255, 255, 0.507);
   border-radius: 20px;
   box-shadow: 2px 2px 5px black;
   display: flex;
+  align-items: center;
+  margin: 0 0 20px 20px;
   &__img {
     display: block;
     width: 230px;
@@ -43,9 +69,15 @@
     font-weight: bold;
     color: $color_1;
     text-shadow: 1px 1px 1px black;
+    margin-bottom: 10px;
+    &-black{
+       color: black;
+       text-shadow: none;
+       font-weight: 400;
+    }
   }
   &__price {
-    margin: 10px 0;
+     margin-bottom: 10px;
     &-bold {
       font-weight: bold;
     }
@@ -54,13 +86,16 @@
     cursor: pointer;
     padding: 5px 10px;
     transition: 0.5s ease;
-    font-size: 16px;
+    font-size: 18px;
     border: none;
     border-radius: 20px;
     &:hover {
       background-color: $color_2;
       transform: scale(1.1);
     }
+  }
+  &__acquisition{
+
   }
 }
 </style>
--- src/components/PlaySpace.vue
+++ src/components/PlaySpace.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="play-space">
-    <div class="top-wrapper">
+    <div class="play-space__top-content">
       <div class="play-space__balans">{{ currentBalans }}</div>
       <switch-radio class="play-space__switch" @change="ringtoneStatus" />
     </div>
-    <business-passive-income />
-    <button class="balans-btn" @click="addMoney()">
-      Заработать
-      <i class="fas fa-coins"></i>
-    </button>
+    <div class="play-space__main-content">
+      <div>
+        <business-passive-income
+          v-for="bisines in allBusiness.slice(0, 4)"
+          :key="bisines.title"
+          :title="bisines.title"
+          :income="bisines.income"
+          :price="bisines.price"
+          :img="bisines.img"
+          @byu-business="byuBusiness"
+        />
+      </div>
+      <div>
+        <button class="balans-btn" @click="addMoney()">
+          Заработать
+          <i class="fas fa-coins"></i>
+        </button>
+      </div>
+      <div>
+        <business-passive-income
+          v-for="bisines in allBusiness.slice(4, 8)"
+          :key="bisines.title"
+          :title="bisines.title"
+          :income="bisines.income"
+          :price="bisines.price"
+          :img="bisines.img"
+          @byu-business="byu - business"
+        />
+      </div>
+    </div>
     <audio src="../assets/audio/click-btn-coin.mp3" ref="audiocoin"></audio>
     <audio
       src="../assets/audio/background-melody.mp3"
@@ -21,6 +46,7 @@
 <script>
 import SwitchRadio from '../components/SwitchRadio.vue';
 import BusinessPassiveIncome from '../components/BusinessPassiveIncome.vue';
+import soundBuy from '../assets/audio/buy.mp3';
 
 export default {
   components: {
@@ -30,6 +56,56 @@ export default {
   data() {
     return {
       balans: 0,
+      allBusiness: [
+        {
+          img: 'flea-market.jpg',
+          title: 'Местовое на барахолке',
+          income: 5,
+          price: 150,
+        },
+        {
+          img: 'shawarma-stall.jpg',
+          title: 'Ларек шаурмы',
+          income: 25,
+          price: 900,
+        },
+        {
+          img: 'mine.jpg',
+          title: 'Рудниковая шахта',
+          income: 125,
+          price: 5400,
+        },
+        {
+          img: 'woodworking.jpg',
+          title: 'Завод по обработке дерева',
+          income: 625,
+          price: 32400,
+        },
+        {
+          img: 'hypermarket.jpg',
+          title: 'Гипермаркет',
+          income: 3125,
+          price: 194400,
+        },
+        {
+          img: 'jewelry-stores.jpg',
+          title: 'Сеть ювелирных магазинов',
+          income: 78125,
+          price: 1166400,
+        },
+        {
+          img: 'avia.jpg',
+          title: 'Авиакомпания',
+          income: 15625,
+          price: 3000000,
+        },
+        {
+          img: 'petroleum.jpg',
+          title: 'Нефтедобывающее производство',
+          income: 390625,
+          price: 6998400,
+        },
+      ],
     };
   },
   methods: {
@@ -46,6 +122,14 @@ export default {
       } else {
         this.$refs.backgroundMelody.pause();
       }
+    },
+    soundBuy() {
+      const audioBuy = new Audio(soundBuy);
+      audioBuy.play();
+    },
+    byuBusiness(event) {
+      console.log(event);
+      this.soundBuy();
     },
   },
   computed: {
@@ -111,17 +195,20 @@ export default {
     }
   }
 
-  &__switch {
+  &__main-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__top-content {
+    display: flex;
+    justify-content: center;
+    margin-left: 80px;
   }
 }
 
 .balans-btn {
   @extend %baseButton;
-}
-
-.top-wrapper {
-  display: flex;
-  justify-content: center;
-  margin-left: 80px;
 }
 </style>
