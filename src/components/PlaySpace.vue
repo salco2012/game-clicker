cbce25fc213d1dd5db8796b13902aa00676745ca
Igor Solodovnick <info@pro-landing.site>
2022-02-07 14:15:46 +0300

    Добавил фоновую музыку с переключателем

--- src/components/PlaySpace.vue
+++ src/components/PlaySpace.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="play-space">
-    <div class="balans">{{ currentBalans }}</div>
-    <button
-      class="balans-btn"
-      @click="
-        addMoney();
-        soundClick();
-      "
-    >
+    <div class="top-wrapper">
+      <div class="play-space__balans">{{ currentBalans }}</div>
+      <switch-radio class="play-space__switch" @change="ringtoneStatus" />
+    </div>
+    <button class="balans-btn" @click="addMoney()">
       Заработать
       <i class="fas fa-coins"></i>
     </button>
-    <audio src="../assets/audio/click-btn-coin.mp3" ref="audiocoin">
-      АУДИО
-    </audio>
+    <audio src="../assets/audio/click-btn-coin.mp3" ref="audiocoin"></audio>
+    <audio
+      src="../assets/audio/background-melody.mp3"
+      loop="true"
+      ref="backgroundMelody"
+    ></audio>
   </div>
 </template>
 
 <script>
+import SwitchRadio from '../components/SwitchRadio.vue';
+
 export default {
+  components: {
+    SwitchRadio,
+  },
   data() {
     return {
       balans: 0,
@@ -27,9 +32,17 @@ export default {
   methods: {
     addMoney() {
       this.balans += 1;
+      this.soundClick();
     },
     soundClick() {
       this.$refs.audiocoin.play();
+    },
+    ringtoneStatus(event) {
+      if (event === true) {
+        this.$refs.backgroundMelody.play();
+      } else {
+        this.$refs.backgroundMelody.pause();
+      }
     },
   },
   computed: {
@@ -67,34 +80,38 @@ export default {
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
-}
 
-.balans {
-  margin: 0 auto;
-  position: relative;
-  height: 40px;
-  width: 300px;
-  background-color: #0000004d;
-  border-radius: 20px;
-  box-shadow: 2px 3px rgba(0, 0, 0, 0.562);
-  font-size: 25px;
-  color: white;
-  font-weight: bold;
-  text-shadow: 2px 2px black;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  &::before {
-    content: '';
-    position: absolute;
-    left: -10px;
-    top: -5px;
-    width: 50px;
-    height: 50px;
-    background-image: url('../assets/img/gold-coin.png');
-    background-size: 50px 50px;
+  &__balans {
+    position: relative;
+    height: 40px;
+    width: 300px;
+    background-color: #0000004d;
+    border-radius: 20px;
+    box-shadow: 2px 3px rgba(0, 0, 0, 0.562);
+    font-size: 25px;
+    color: white;
+    font-weight: bold;
+    text-shadow: 2px 2px black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 20px 100px 0;
+    &::before {
+      content: '';
+      position: absolute;
+      left: -10px;
+      top: -5px;
+      width: 50px;
+      height: 50px;
+      background-image: url('../assets/img/gold-coin.png');
+      background-size: 50px 50px;
+    }
+  }
+
+  &__switch {
   }
 }
+
 .balans-btn {
   cursor: pointer;
   background-color: gold;
@@ -116,5 +133,11 @@ export default {
     box-shadow: none;
     top: 10px;
   }
+}
+
+.top-wrapper {
+  display: flex;
+  justify-content: center;
+  margin-left: 80px;
 }
 </style>
